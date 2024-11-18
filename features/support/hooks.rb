76318d725cbbfc17746a5d58636f5997a6e2dde3
poornachander143 <poornachander.v@gmail.com>
2024-11-18 10:53:08 +0530

    changes

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -4,7 +4,7 @@ require_relative '../pages/drsignuppage.rb'
 #require_relative 'C:\Users\LENOVO\OneDrive\Desktop\cucumberproject\pages\drsignuppage.rb'
 require_relative 'C:\Users\LENOVO\OneDrive\Desktop\cucumberproject\pages\facebookexample1page.rb'
 require_relative 'C:\Users\LENOVO\OneDrive\Desktop\cucumberproject\pages\practicepage.rb'
-require_relative 'C:\Users\LENOVO\OneDrive\Desktop\cucumberproject\pages\hiperfiberpage.rb'
+#require_relative 'C:\Users\LENOVO\OneDrive\Desktop\cucumberproject\pages\hiperfiberpage.rb'
 Before do |scenario|
   DataMagic.load_for_scenario(scenario)
   @browser = Selenium::WebDriver.for :chrome
