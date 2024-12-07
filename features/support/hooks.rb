0be76452757ec692ed2a0a0abcaa1901b1fdd18a
poornachander143 <poornachander.v@gmail.com>
2024-12-08 00:24:33 +0530

    first attempt

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,5 +1,5 @@
 require 'selenium-webdriver'
-require_relative './config/environment.rb' 
+#require_relative './config/environment.rb' 
 #require_relative '../pages/hyperfiberpage.rb'
 require_relative 'C:\Users\LENOVO\OneDrive\Desktop\cucumberproject\pages\drsignuppage.rb'
 require_relative 'C:\Users\LENOVO\OneDrive\Desktop\cucumberproject\pages\facebookexample1page.rb'
